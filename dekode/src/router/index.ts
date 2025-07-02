8ad6402cfdaaf2b3325e1a1d8090e742dfa0cf40
Joao Victor <belfort.joao216@gmail.com>
2025-07-02 14:43:41 -0300

    Fix missing router export

---
+++ dekode/src/router/index.ts
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Dashboard from '../pages/Dashboard/Dashboard';
+import Editor from '../pages/Editor/Editor';
+import AppLayout from '../components/Layout/AppLayout';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+      {
+        path: '/editor',
+        element: <Editor />,
+      },
+    ],
+  },
+]);
